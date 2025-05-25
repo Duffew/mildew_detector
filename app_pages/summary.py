2c8c9ef63e4619a31c20a74633213183c22a7cfd
Duffew <pbsvimqx@students.codeinstitute.net>
2025-05-25 17:09:53 +0100

    feat: add detail to Project Summary streamlit page

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -1,5 +1,32 @@
 import streamlit as st
 
 def show():
+    """Project Summary Page for Mildew Detection"""
     st.title("📜 Project Summary")
-    st.write("This page will contain an overview of the project.")
+    
+    st.subheader("🌳 Project Background")
+    st.write("""
+    Cherry trees are susceptible to a fungus known as podosphaera clandestine, which presents as a powdery mildew.
+    
+    - 🌧️ Fungus spores, which winter on or close to the trees, are released in response to rain or irrigation and attach themselves to leaves or new shoots.
+    - 🌱 The spores are most active during the spring, and an infected tree will show signs of mildew within 4-6 weeks of new buds breaking.
+    - 🍀 Powdery mildew usually appears on the leaves of cherry trees and may also be present on stems and the fruit.
+    - ⚠️ If left untreated, powdery mildew will reduce the yield of a cherry tree, is contagious for other plants, and will become a perennial problem.
+    """)
+
+    st.subheader("🖼️ Project Dataset")
+    st.write("""
+    - 🍀 The project makes use of images of cherry leaves categorized as either ‘healthy’ or suffering from ‘powdery mildew’.  
+    - 📷 There are **2,104 images** in each category.
+    """)
+
+    st.subheader("💼 Business Requirements")
+    st.write("""
+    - 🔎 The client would like to see a study of the images.
+    - 🤖 The client would like to see a machine learning prediction on whether an image of a cherry leaf shows a healthy leaf or mildew-affected.
+    """)
+
+    st.subheader("📖 Further Reading")
+    st.markdown("[📄 Mildew Detector Business Case](https://github.com/Duffew/mildew_detector/tree/main#ml-business-case-assessment)")
+    st.markdown("[🍒 Powdery Mildew in Sweet Cherries](https://www.bctfpg.ca/pest_guide/info/101/Powdery_Mildew_Sweet_Cherries#:~:text=Powdery%20mildew%20of%20cherry%20is%20caused%20by%20the%20fungus%20Podosphaera%20clandestina.&text=On%20leaves%2C%20powdery%20mildew%20appears,often%20puckered%20or%20distorted%20(Fig.))")
+    st.markdown("[🌱 Powdery Mildew Information](https://www.wmmga.org/content.aspx?page_id=22&club_id=101643&module_id=228931#:~:text=If%20you've%20had%20powdery,resistant%20varieties%20have%20been%20developed.)")
